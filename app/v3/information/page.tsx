a81f929b1c158e237f2ebe69d320e5b653db6b45
Philip <philipandersson27@gmail.com>
2024-09-26 19:43:22 +0200

    Improve text on information page. Minor changes to the text on the main page.

--- app/v3/information/page.tsx
+++ app/v3/information/page.tsx
@@ -11,47 +11,22 @@ export default function Information() {
 
   return (
     <main className="bg-[#0d1421] w-screen min-h-screen overflow-x-hidden p-4 sm:p-6">
-      <div className="w-full flex justify-center mb-4 sm:mb-6">
-        <Link href="/v3">
-          <svg
-            className="fill-white hover:fill-gray-400 size-10"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              <path
-                d="M9.66088 8.53078C9.95402 8.23813 9.95442 7.76326 9.66178 7.47012C9.36913 7.17698 8.89426 7.17658 8.60112 7.46922L9.66088 8.53078ZM4.47012 11.5932C4.17698 11.8859 4.17658 12.3607 4.46922 12.6539C4.76187 12.947 5.23674 12.9474 5.52988 12.6548L4.47012 11.5932ZM5.51318 11.5771C5.21111 11.2936 4.73648 11.3088 4.45306 11.6108C4.16964 11.9129 4.18475 12.3875 4.48682 12.6709L5.51318 11.5771ZM8.61782 16.5469C8.91989 16.8304 9.39452 16.8152 9.67794 16.5132C9.96136 16.2111 9.94625 15.7365 9.64418 15.4531L8.61782 16.5469ZM5 11.374C4.58579 11.374 4.25 11.7098 4.25 12.124C4.25 12.5382 4.58579 12.874 5 12.874V11.374ZM15.37 12.124V12.874L15.3723 12.874L15.37 12.124ZM17.9326 13.1766L18.4614 12.6447V12.6447L17.9326 13.1766ZM18.25 15.7351C18.2511 16.1493 18.5879 16.4841 19.0021 16.483C19.4163 16.4819 19.7511 16.1451 19.75 15.7309L18.25 15.7351ZM8.60112 7.46922L4.47012 11.5932L5.52988 12.6548L9.66088 8.53078L8.60112 7.46922ZM4.48682 12.6709L8.61782 16.5469L9.64418 15.4531L5.51318 11.5771L4.48682 12.6709ZM5 12.874H15.37V11.374H5V12.874ZM15.3723 12.874C16.1333 12.8717 16.8641 13.1718 17.4038 13.7084L18.4614 12.6447C17.6395 11.8276 16.5267 11.3705 15.3677 11.374L15.3723 12.874ZM17.4038 13.7084C17.9435 14.245 18.2479 14.974 18.25 15.7351L19.75 15.7309C19.7468 14.572 19.2833 13.4618 18.4614 12.6447L17.4038 13.7084Z"
-                // fill="#ffffff"
-              ></path>
-            </g>
-          </svg>
-        </Link>
-      </div>
-
       <div className="flex justify-center">
         <div className="flex flex-col lg:flex-row gap-8 sm:gap-4 max-w-[1700px]">
-          <div className="bg-slate-50 text-neutral-800 rounded-xl px-4 sm:px-6 pt-6 sm:pt-8 pb-3 sm:pb-5 lg:w-1/2">
+          <div className="bg-slate-50 text-neutral-800 rounded-lg px-4 sm:px-6 pt-6 sm:pt-8 pb-3 sm:pb-5 lg:w-1/2">
             <h2 className="font-bold text-xl sm:text-2xl">Compound Interest</h2>
             <p className="mt-1 sm:mt-2">
-              The financial principle of compound interest is powerful, and
-              everyone should know about it. By learning about compound interest
-              everyone can increase their future savings. With compound
+              The powerful financial principle of compound interest, from which
+              everybody can benefit after knowing about it. With compound
               interest, you can make your money work for you instead of the
               other way around. Allegedly, Albert Einstein called compound
               interest the eighth wonder of the world.
             </p>
             <p className="mt-3 sm:mt-4">
               The way compound interest works is that you earn interest not only
-              on your initial amount but also interest on the previously
-              accumulated amount of interest over time. One common way to
-              explain compound interest is like creating a snowball. In the
+              on your initial amount (principal) but also interest on the
+              previously accumulated amount of interest over time. One common
+              way to explain compound interest is like making a snowball. In the
               beginning, when rolling a snowball in the snow it grows slowly but
               the more snow it picks up, the larger the snowball becomes and the
               faster it grows in size. That is why compound interest is often
@@ -60,9 +35,9 @@ export default function Information() {
             <p className="mt-3 sm:mt-4">
               You can compare compound interest to simple interest where you
               only earn interest on the initial amount. Given you don't save any
-              more money, you will earn the same amount of interest. The money
-              will linearly grow instead of exponentially which it does with
-              compound interest.
+              more money, you will earn the same amount of interest every
+              interval. The money will grow linearly instead of exponentially
+              which it does with compound interest.
             </p>
             <p className="mt-3 sm:mt-4">
               When it comes to compound interest, the most important factor is
@@ -72,27 +47,27 @@ export default function Information() {
               interest, it is important to start saving early. To achieve the
               same returns when saving smaller amounts over a longer period, you
               would need to significantly increase the initial amount if saving
-              over a shorter period. So saving a small amount every month over a
-              period of decades will yield a high return no matter what.
+              over a shorter period. So, it is worth to save a small amount
+              every month over a period of decades.
             </p>
             <p className="mt-3 sm:mt-4">
               Mathematically, the formula for compound interest is an
               exponential function that grows at an increasing rate for each
-              period (see more in “The Formula”).
+              interval (see more in “The Formula”).
             </p>
             <p className="mt-3 sm:mt-4">
-              There are different compound interest periods, for instance,
+              There are different compound interest intervals, for instance
               annually, quarterly, and monthly. The more frequently you compound
-              your savings, the higher the returns over time. The interval used
-              in CompoundVision is annually which was decided because it is the
-              most common.
+              your savings, the higher the returns over time. Annual is used in
+              CompoundVision, which was chosen because it is the most common
+              interval.
             </p>
             <p className="mt-3 sm:mt-4">
               Monthly contributions, which you can include in the CompoundVision
-              calculator, will also greatly increase your future amount of
-              savings. This is because you will earn compound interest on these
-              contributions as well. As for the frequency, the monthly
-              contributions are compounded annually.
+              calculator, will also greatly impact the long-term return. This is
+              because you will earn compound interest on these contributions as
+              well. As for the frequency, the monthly contributions are
+              compounded annually.
             </p>
 
             <h3 className="font-bold text-lg mt-3">Example:</h3>
@@ -125,59 +100,20 @@ export default function Information() {
             <p className="mt-2">
               As you can see the amount of interest grows each year.
             </p>
-            <h3 className="font-bold text-lg mt-3">
-              Contrary viewpoint - Debt:
-            </h3>
+            <h3 className="font-bold text-lg mt-3">Debt:</h3>
             <p>
               Compound interest also applies to debt. If you have a loan and
               don't pay the interest, this will be added to the debt and
               increase the amount of interest you need to pay in the future. You
               could say it is the reverse as if you saved money with compound
               interest. The debt will increasingly grow, and it gets harder to
-              pay it off. That's why it's crucial to pay it off as quickly as
+              pay it off. That's why it's crucial to pay off debt as quickly as
               possible.
             </p>
           </div>
 
           <div className="flex flex-col gap-y-8 sm:gap-y-4 lg:w-1/2">
-            <div className="bg-slate-50 text-neutral-800 rounded-xl px-4 sm:px-6 pt-6 sm:pt-8 pb-3 sm:pb-5 lg:grow">
-              <h2 className="font-bold text-xl sm:text-2xl">Inflation</h2>
-              <p className="mt-1 sm:mt-2">
-                With the CompoundVision calculator, you can enter the annual
-                inflation rate. This really shows how much of an impact
-                inflation has on each person's savings. There is a common
-                misconception when considering the future value of money.
-              </p>
-              <p className="mt-3 sm:mt-4">
-                Inflation is a general increase in prices of goods and services
-                over time. This leads to a loss of value of the savings you
-                have. Let’s say you have $100, and there's a product you want to
-                buy that costs exactly $100. However, when you return to buy the
-                product later, it now costs $102 (2% increase). Even though you
-                still have the same $100, you can no longer afford the product
-                because its price has increased. This means the purchasing power
-                of your money has decreased, and that you need more money to buy
-                the same product.
-              </p>
-              <p className="mt-3 sm:mt-4">
-                There are two key terms used to describe future monetary value:
-                nominal and real. Nominal refers to the future value without
-                accounting for inflation, while real value is adjusted for
-                inflation, reflecting the true purchasing power.
-              </p>
-              <p className="mt-3 sm:mt-4">
-                Without any interest on your savings, it will decrease in value
-                over time. Try the calculator with an interest rate of zero and
-                some inflation rate, and you will see the real future value of
-                your savings. To counteract the impact of inflation, it is
-                important to obtain a minimum of the same annual interest rate
-                on your savings as the annual inflation rate. With compound
-                interest, it will be easier to beat inflation and increase your
-                chance of securing your future prosperity.
-              </p>
-            </div>
-
-            <div className="bg-slate-50 text-neutral-800 rounded-xl px-4 sm:px-6 pt-6 sm:pt-8 pb-3 sm:pb-5 lg:grow">
+            <div className="bg-slate-50 text-neutral-800 rounded-lg px-4 sm:px-6 pt-6 sm:pt-8 pb-3 sm:pb-5 lg:grow">
               <MathJaxContext>
                 <h2 className="font-bold text-xl sm:text-2xl">The Formula</h2>
                 <p className="mt-1 sm:mt-2 mb-3 sm:mb-4">
@@ -205,7 +141,7 @@ export default function Information() {
                 <p className="mt-3 sm:mt-4">
                   The formula is an exponential function that increasingly grows
                   the greater n becomes (increasingly grows over time). This
-                  formula is derived from a more complex one, where the period
+                  formula is derived from a more complex one, where the interval
                   has been set to one year.
                 </p>
                 <h3 className="font-bold text-lg mt-3 mb-2 sm:mb-3">
@@ -239,17 +175,75 @@ export default function Information() {
                   compound interest on only the monthly contributions. These
                   parts are added together to obtain the future value. This
                   formula has also been derived from a more complex one, where
-                  the period has been set to one year.
+                  the interval has been set to one year and the contributions to
+                  be monthly.
                 </p>
               </MathJaxContext>
+            </div>
+
+            <div className="bg-slate-50 text-neutral-800 rounded-lg px-4 sm:px-6 pt-6 sm:pt-8 pb-3 sm:pb-5 lg:grow">
+              <h2 className="font-bold text-xl sm:text-2xl">Inflation</h2>
+              <p className="mt-1 sm:mt-2">
+                With the CompoundVision calculator, you can enter an annual
+                inflation rate. This really shows how much of an impact
+                inflation has on each person's future savings. There is a common
+                misconception when considering the future value of money, which
+                is also intended to be emphasized with this calculator.
+              </p>
+              <p className="mt-3 sm:mt-4">
+                Inflation is a general increase in prices of goods and services
+                over time. This leads to a loss of value of the savings you
+                have. Let’s say you have $100, and there's a product you want to
+                buy that costs exactly $100. However, when you return to buy the
+                product later, it now costs $102 (2% increase). Even though you
+                still have the same $100, you can no longer afford the product
+                because its price has increased. This means the purchasing power
+                of your money has decreased, and that you need more money to buy
+                the same product.
+              </p>
+              <p className="mt-3 sm:mt-4">
+                There are two key terms used to describe future monetary value:
+                nominal and real. Nominal refers to the future value without
+                accounting for inflation, while real value is adjusted for
+                inflation, reflecting the true purchasing power.
+              </p>
+              <p className="mt-3 sm:mt-4">
+                Without any interest on your capital, it will decrease in value
+                over time. Try the calculator with an interest rate of zero and
+                some inflation rate (e.g., 2%), and you will see the real future
+                value of your savings. To counteract the impact of inflation, it
+                is important to obtain a minimum of the same annual interest
+                rate on your savings as the national annual inflation rate. With
+                compound interest, it will be easier to beat inflation.
+              </p>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="mt-10 sm:mt-12 flex justify-center">
+      <div className="flex flex-col items-center mt-4">
+        <div className="inline-flex justify-center">
+          <Link href="/v3">
+            <svg
+              className="fill-white hover:fill-gray-400 size-10 transition-all"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <g id="SVGRepo_iconCarrier">
+                <path d="M9.66088 8.53078C9.95402 8.23813 9.95442 7.76326 9.66178 7.47012C9.36913 7.17698 8.89426 7.17658 8.60112 7.46922L9.66088 8.53078ZM4.47012 11.5932C4.17698 11.8859 4.17658 12.3607 4.46922 12.6539C4.76187 12.947 5.23674 12.9474 5.52988 12.6548L4.47012 11.5932ZM5.51318 11.5771C5.21111 11.2936 4.73648 11.3088 4.45306 11.6108C4.16964 11.9129 4.18475 12.3875 4.48682 12.6709L5.51318 11.5771ZM8.61782 16.5469C8.91989 16.8304 9.39452 16.8152 9.67794 16.5132C9.96136 16.2111 9.94625 15.7365 9.64418 15.4531L8.61782 16.5469ZM5 11.374C4.58579 11.374 4.25 11.7098 4.25 12.124C4.25 12.5382 4.58579 12.874 5 12.874V11.374ZM15.37 12.124V12.874L15.3723 12.874L15.37 12.124ZM17.9326 13.1766L18.4614 12.6447V12.6447L17.9326 13.1766ZM18.25 15.7351C18.2511 16.1493 18.5879 16.4841 19.0021 16.483C19.4163 16.4819 19.7511 16.1451 19.75 15.7309L18.25 15.7351ZM8.60112 7.46922L4.47012 11.5932L5.52988 12.6548L9.66088 8.53078L8.60112 7.46922ZM4.48682 12.6709L8.61782 16.5469L9.64418 15.4531L5.51318 11.5771L4.48682 12.6709ZM5 12.874H15.37V11.374H5V12.874ZM15.3723 12.874C16.1333 12.8717 16.8641 13.1718 17.4038 13.7084L18.4614 12.6447C17.6395 11.8276 16.5267 11.3705 15.3677 11.374L15.3723 12.874ZM17.4038 13.7084C17.9435 14.245 18.2479 14.974 18.25 15.7351L19.75 15.7309C19.7468 14.572 19.2833 13.4618 18.4614 12.6447L17.4038 13.7084Z" />
+              </g>
+            </svg>
+          </Link>
+        </div>
         <button
-          className="hover:bg-neutral-200 text-white hover:text-black bg-opacity-70 rounded-lg p-2 transition-all"
+          className="mt-1 text-white hover:text-gray-400 rounded-lg p-2 transition-all"
           onClick={scrollToTop}
         >
           To top
