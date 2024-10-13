c9d9f8dc966e8c2b06a1a2be7882d18599ecac64
Philip <philipandersson27@gmail.com>
2024-10-13 13:32:53 +0200

    Adjust responsive breakpoint for monthly contribution formula.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       screens: {
-        formula: "350px",
+        formula: "420px",
       },
     },
   },
